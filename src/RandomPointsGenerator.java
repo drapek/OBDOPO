2cbc791b236689c41f6fdf9c5edeea2de1806a13
drapek <drapek39@wp.pl>
2015-10-26 22:31:03 +0100

    first scrach of random points generator (necessery for future development)

---
+++ src/RandomPointsGenerator.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+/**
+ * Created by drapek on 26.10.15.
+ */
+
+public class RandomPointsGenerator {
+    double minX, minY, maxX, maxY;
+    double howManyPointsCreate;
+    public Point[] randomizedPoints;
+
+    public RandomPointsGenerator(double minX, double minY, double maxX, double maxY, int howManyGenerate) {
+
+    }
+}
